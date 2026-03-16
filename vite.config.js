8c3ba66a6ec87bf884d8ba3c5a84934b3996b7e1
josselinesot <josselinesot@gmail.com>
2026-03-15 23:08:51 -0600

    Merge rama Gulmaro en Josseline

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-<<<<<<< HEAD
+
             input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/facturacion.js','resources/css/CRM/leads.css','resources/css/CRM/prospectos.css','resources/css/CRM/comisiones.css'],
-=======
+
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
@@ -15,7 +15,6 @@ export default defineConfig({
                 'resources/css/Control Admin/horario-edit.css',
                 'resources/js/horario-edit.js',
             ],
->>>>>>> origin/Gulmaro
             refresh: true,
         }),
         tailwindcss(),
